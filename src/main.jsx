1fe272aa6c715ccc49d921ac1d19dc1d5716a90b
andkstrr <andikasatrionurcahyo@gmail.com>
2025-02-22 21:11:28 +0700

    Initial commit

---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import Home from './pages/Home.jsx'
+import Button from './components/Button.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+    <Home />
+  </StrictMode>,
+)
---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import Button from '../components/Button'
+
+const Home = () => {
+  return (
+    <div>   
+    </div>
+  )
+}
+
+export default Home
